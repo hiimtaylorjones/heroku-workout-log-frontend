113979314751891edccd76f2b4361da899e59ef5
Taylor Jones <taylor.jones@salesforce.com>
2020-04-01 17:07:56 -0400

    working through navigation css

--- app/models/workout.js
+++ app/models/workout.js
@@ -1,8 +1,11 @@
 import Model, { attr, hasMany } from '@ember-data/model';
 export default class WorkoutModel extends Model {
+  // Primative Data Attributes
+  @attr('string') title;
   @attr('string') classification;
   @attr('string') condition;
   @attr('string') notes;
 
+  // Relationships
   @hasMany('units') units;
 }
