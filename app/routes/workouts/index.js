83720071eab51ee24b3bee35cecac7ce329363e9
Taylor Jones <taylor.jones@salesforce.com>
2020-09-21 16:36:38 -0400

    securing routes

--- app/routes/workouts/index.js
+++ app/routes/workouts/index.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default class WorkoutsIndexRoute extends Route {
+export default class WorkoutsIndexRoute extends Route.extend(AuthenticatedRouteMixin) {
   @service store;
 
   model() {
