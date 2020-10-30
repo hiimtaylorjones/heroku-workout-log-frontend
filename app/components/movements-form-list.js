a9ba47720f6e9e5cfbd641c0028cf0d0cbaf9c29
Taylor Jones <taylor.jones@salesforce.com>
2020-10-30 16:10:10 -0400

    implementing nesting of ember forms

--- app/components/movements-form-list.js
+++ app/components/movements-form-list.js
@@ -5,16 +5,21 @@ import { inject as service } from '@ember/service';
 
 export default class MovementsFormListComponent extends Component {
   @service store;
-
+  
   @tracked movements;
-  @tracked workout;
   @tracked isLoading = false;
+
+  constructor(owner, args) {
+    super(owner, args);
+    this.movements = this.args.movements;
+  }
+
+  
 
   @action addMovement() {
     this.isLoading = true;
     let move = this.movements;
     let newMovement = this.store.createRecord('movement');
-    debugger;
     move.addObject(newMovement);
     this.movements = move;
     this.isLoading = false;
